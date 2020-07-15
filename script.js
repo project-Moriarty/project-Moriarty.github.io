61c1634640839a5266879bf61dfd3a4c67ca6903
project-Moriarty <68346059+project-Moriarty@users.noreply.github.com>
2020-07-15 20:18:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function soon_button () {
+    alert('Проект Мориарти - скоро.');
+}
+
+function soon_eng_button () {
+    alert('Project Moriarty - soon.');
+}
+
+function rus_vers_button() {
+    alert('Это уже русская версия сайта!');
+}
+
+function eng_vers_button() {
+    alert('It is already english version of site!');
+}
+
+function dark_theme_rus_button() {
+    alert('Темная тема уже включена!');
+}
+
+function light_theme_rus_button() {
+    alert('Светлая тема уже включена!');
+}
+
+function dark_theme_eng_button() {
+    alert('It is already dark theme!');
+}
+
+function light_theme_eng_button() {
+    alert('It is already light theme!');
+}
