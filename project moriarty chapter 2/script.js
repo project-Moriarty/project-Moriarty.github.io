358da15be636a61329cd8dd22247782815d5901f
project-Moriarty <68346059+project-Moriarty@users.noreply.github.com>
2020-10-25 14:11:56 +0400

    Add files via upload

---
+++ project moriarty chapter 2/script.js
@@ -0,0 +1,11 @@
+function rus_vers_button () {
+    alert('Русский язык уже включен!');
+}
+
+function dark_theme_rus_button() {
+    alert('Темная тема уже включена!');
+}
+
+function themes_funct() {
+    alert('Функция временно недоступна!');
+}
